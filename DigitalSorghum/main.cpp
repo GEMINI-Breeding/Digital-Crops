ad4c94d9fb3d9ec110a33f2e0aa140e1c979cd09
Heesup Yun <hspyun@ucdavis.edu>
2023-07-27 12:49:04 -0700

    Planted more Sorghum

--- DigitalSorghum/main.cpp
+++ DigitalSorghum/main.cpp
@@ -1,21 +1,22 @@
 #include "Visualizer.h"
 #include "CanopyGenerator.h"
 
+#include "main.h"
+
 using namespace helios;
 
-int make_field(Context &context){
 
-    const float bed_height = 0.2;
+int make_field(Context &context){
 
     int n_beds = 2; //6
     int n_rows = 2; // 20
 
-    std::vector<uint> UUIDs = context.loadOBJ("../../DigitalSorghum/obj/dirt_rocks.obj", make_vec3(0,0,0),bed_height,nullrotation,RGB::white);
+    std::vector<uint> UUIDs = context.loadOBJ("../../DigitalSorghum/obj/dirt_rocks.obj", make_vec3(0,0,0), BED_HEIGHT, nullrotation, RGB::white);
 
     for(int bed = 0;bed < n_beds;bed++){
         for(int row=0;row<n_rows;row++){
-            float x = bed*bed_height*10;
-            float y = row*bed_height*20;
+            float x = bed * BED_WIDTH;
+            float y = row * BED_LENGTH;
             float z = 0;
             std::vector<uint> UUIDs_copy = context.copyPrimitive(UUIDs);
             context.translatePrimitive(UUIDs_copy, make_vec3(x,y,z));
@@ -23,7 +24,34 @@ int make_field(Context &context){
     }
 }
 
+int plant_sorghum(Context &context){
+    // Canopy generator model
+    CanopyGenerator canopygenerator(&context);
 
+    //Declare the parameter set for VSP grapevine
+    SorghumCanopyParameters parameters;
+    parameters.plant_count = make_int2(2,8);
+
+
+    //Variable defining the location of the plant
+//    vec3 origin(0,0,0);
+    //Add the sorghum geometry to the Context
+//    canopygenerator.sorghum( parameters, origin );
+
+    int n_beds = 2; //6
+    int n_rows = 2; // 20
+    for(int bed = 0;bed < n_beds;bed++){
+        for(int row=0;row<n_rows;row++){
+            float x = bed * BED_WIDTH;
+            float y = row * BED_LENGTH;
+            float z = 0;
+            vec3 origin(x,y,z);
+            parameters.canopy_origin = origin;
+            //canopygenerator.sorghum( parameters, origin ); // Gererate a single Sorhgum plant
+            canopygenerator.buildCanopy( parameters);
+        }
+    }
+}
 
 int main(){
     Context context;
@@ -31,19 +59,8 @@ int main(){
     // OBJ 3D Model
     make_field(context);
 
-    // Canopy generator model
-    CanopyGenerator canopygenerator(&context);
-
-    //Declare the parameter set for VSP grapevine
-    SorghumCanopyParameters parameters;
-    parameters.plant_count = make_int2(1,1);
-
-    //Variable defining the location of the plant
-    vec3 origin(0,0,0);
-
-    //Add the sorghum geometry to the Context
-    canopygenerator.sorghum( parameters, origin );
-
+    // Plant sorghum
+    plant_sorghum(context);
 
     // Set Visualizer
     Visualizer visualizer(800);
